9d1a0a0135cbf2bf493fba99cff3bb853b8351fa
ridwantajudeen <ridwantajudeen8@gmail>
2024-09-25 19:34:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Header from './dashboard/Header';
+import Dashboard from './dashboard/Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Dashboard/>
     </div>
   );
 }
