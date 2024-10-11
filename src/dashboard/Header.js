3ed9adc26b4e358f8936a71dbcb28df354173355
ridwantajudeen <ridwantajudeen8@gmail>
2024-10-11 12:37:20 +0100

    update

--- src/dashboard/Header.js
+++ src/dashboard/Header.js
@@ -1,27 +1,65 @@
-import React from 'react'
-import logo from './images/motion-logo.png'
-import burger from './images/burger1.JPG'
-function Header(){
-    return(
-        <div className='Header'
+import React, { useState, useEffect } from 'react';
+import logo from './images/motion-logo.png';
+import burger from './images/burger1.JPG';
+
+
+function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const headerHeight = document.querySelector('.Header').offsetHeight;
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > headerHeight) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Main Header */}
+      <div
+        className="Header"
         style={{
-            backgroundImage: `url(${burger})`,
-            backgroundSize: 'cover',
-            height: '300px',
-            filter: 'brightness(0.8)',
+          backgroundImage: `url(${burger})`,
+          backgroundSize: 'cover',
+          height: '300px',
+          filter: 'brightness(0.8)',
         }}
-        >
-            <div className='head'>
-            <img src={logo} className='logo' alt='logo' />
-            <button>Login</button>
-            </div>
-            <div className='location'>
-                <h1>Where do you want us to deliver to?</h1>
-                <input type='text' placeholder='ENTER YOUR ADDRESS HERE'/><br />
-                <button>SEARCH</button>
-            </div>
+      >
+        <div className="head">
+          <img src={logo} className="logo" alt="logo" />
+          <button>Login</button>
         </div>
-    )
+        <div className="location">
+          <h1>Where do you want us to deliver to?</h1>
+          <input type="text" placeholder="ENTER YOUR ADDRESS HERE" /><br />
+          <button>SEARCH</button>
+        </div>
+      </div>
+
+      {/* Sticky Header */}
+      <div className={`sticky-header ${isSticky ? 'visible' : ''}`}>
+      <div className="location-stick">
+          
+          <input type="text-stick" placeholder="ENTER YOUR ADDRESS HERE" /><br />
+          <button>SEARCH</button>
+        </div>
+      </div>
+
+     
+    </div>
+  );
 }
 
 export default Header;
