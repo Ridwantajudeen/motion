a75e21dd693dfcf7889ddfd7a568d11a3bb64db8
ridwantajudeen <ridwantajudeen8@gmail>
2024-10-11 02:56:21 +0100

    update

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
+import Middle from "./Middle";
 
 function Dashboard(){
     return(
         <div className="Dashboard">
             <Header/>
             <Hero/>
+            <Middle/>
         </div>
     )
 }
---
+++ src/dashboard/Middle.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function Middle() {
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const handleCopy = () => {
+    const textToCopy = "firstTimer210";
+    
+    // Use the Clipboard API to copy the text
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      setCopySuccess('Copied!');
+    }).catch(err => {
+      setCopySuccess('Failed to copy');
+    });
+  };
+
+  return (
+    <div className='middle'
+    style={{
+        display: 'grid',
+        placeItems: 'center',
+       
+      }}>
+        <div id='middle'>
+    <h2>Enjoy our discount on your first order</h2>
+    <p>Click the button below to copy your coupon code and paste at the point of checkout</p>
+
+    <div>
+      {/* The text that will be copied when clicked */}
+      <button onClick={handleCopy} >
+        Cupon Code
+      </button>
+
+      {/* Display success or failure message */}
+      {copySuccess && <p>{copySuccess}</p>}
+    </div>
+    <p>Enjoy 15% off your first delivery fee. Ball on us😘</p>
+    </div>
+    </div>
+  );
+}
+
+export default Middle;
